3d52d9746188e0b97dbf37409701d931306c10aa
Rahul <rahsolan@cisco.com>
2018-12-04 17:02:15 +0530

    First commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'This is a test recipe',
+     'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg'),
+     new Recipe('A Test ', 'This is a test ',
+     'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg') 
+  ];
+
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
